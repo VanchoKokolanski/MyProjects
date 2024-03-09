068fcd5f5643e84baee8ecd04880a1dbf1e71f82
Vancho Kokolanski <vanka.k91@gmail.com>
2024-03-09 11:24:40 +0100

    Layout Sidebar, Mobile Navigation, Database & Models Setup

--- saas_app/src/app/(root)/layout.tsx
+++ saas_app/src/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import MobileNav from "@/components/shered/MobileNav";
 import Sidebar from "@/components/shered/Sidebar";
 import React from "react";
 
@@ -5,6 +6,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <Sidebar />
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
