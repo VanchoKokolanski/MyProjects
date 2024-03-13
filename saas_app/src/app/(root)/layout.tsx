e2488d23f81305c9f6d5e424a238fbfedeffc209
Vancho Kokolanski <vanka.k91@gmail.com>
2024-03-13 09:46:10 +0100

    implement webhooks

--- saas_app/src/app/(root)/layout.tsx
+++ saas_app/src/app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-import MobileNav from "@/components/shered/MobileNav";
-import Sidebar from "@/components/shered/Sidebar";
+import MobileNav from "@/components/shared/MobileNav";
+import Sidebar from "@/components/shared/Sidebar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
